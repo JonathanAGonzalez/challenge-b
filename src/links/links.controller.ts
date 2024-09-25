f543c0b3994bfa68378019624a0aaab0733fd0c0
Jona <jonathang@hotmail.com.ar>
2024-09-25 14:47:19 -0300

    feat: add links module with controller service and DTOs

---
+++ src/links/links.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
+import { LinksService } from './links.service';
+import { CreateLinkDto } from './dto/create-link.dto';
+import { UpdateLinkDto } from './dto/update-link.dto';
+
+@Controller('links')
+export class LinksController {
+  constructor(private readonly linksService: LinksService) {}
+
+  @Post()
+  create(@Body() createLinkDto: CreateLinkDto) {
+    return this.linksService.create(createLinkDto);
+  }
+
+  @Get()
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.linksService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateLinkDto: UpdateLinkDto) {
+    return this.linksService.update(+id, updateLinkDto);
+  }
+}

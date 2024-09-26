b3df0489b4b6bc8eb75ec2c09042380ab50a0695
Jona <jonathang@hotmail.com.ar>
2024-09-25 22:10:15 -0300

    feat: enhance Link entity with additional properties and Mongoose schema

--- src/links/entities/link.entity.ts
+++ src/links/entities/link.entity.ts
@@ -1,1 +1,29 @@
-export class Link {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type LinkDocument = Link & Document;
+@Schema({ timestamps: true })
+export class Link {
+  @Prop({ isRequired: true })
+  url: string;
+
+  @Prop({ default: 0, isRequired: false })
+  redirectCount: number;
+
+  @Prop({ isRequired: false })
+  maskedUrl: string;
+
+  @Prop({ isRequired: false })
+  shortenedUrl: string;
+
+  @Prop({ default: false, isRequired: false })
+  isDisabled: boolean;
+
+  @Prop({ isRequired: false })
+  expirationDate?: Date;
+
+  @Prop({ isRequired: false })
+  password?: string;
+}
+
+export const LinkSchema = SchemaFactory.createForClass(Link);
